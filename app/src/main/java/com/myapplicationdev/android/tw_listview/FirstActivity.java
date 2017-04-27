753d75541f5ff7fde648ffaf55ee74382a78283c
nickngweipeng <15056201@myrp.edu.sg>
2017-04-27 15:14:37 +0800

    15056201

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,32 +1,50 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class FirstActivity extends AppCompatActivity {
 
     ListView lv;
-    ArrayList<Year> al;
-    YearAdapter aa;
+    ArrayList<String> al;
+    ArrayAdapter<String> aa;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lv = (ListView)this.findViewById(R.id.lvCourse);
-
-        al = new ArrayList<Year>();
-        al.add(new Year("Year 1"));
-        al.add(new Year("Year 2"));
-        al.add(new Year("Year 3"));
+        lv = (ListView)findViewById(R. id. lvYear);
+        tv = (TextView)findViewById(R. id. textview);
 
 
-        aa = new YearAdapter(this, R.layout.activity_second, al);
+        al = new ArrayList<String>();
+        al.add("Year 1");
+        al.add("Year 2");
+        al.add("Year 3");
+
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
         lv.setAdapter(aa);
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String year = al.get(position).toString();
+                Intent i = new Intent(FirstActivity.this, SecondActivity.class);
+                i.putExtra("year", year);
+                startActivity(i);
+            }
+        });
+
     }
 }
